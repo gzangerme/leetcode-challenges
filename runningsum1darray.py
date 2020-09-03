92d35fcc958dd9dea6a268db21eb081854f4e2e1
Gabriel Zangerme <gzangerme@gmail.com>
2020-09-03 17:57:19 -0300

    1480. Running Sum of 1d Array
    1480. Running Sum of 1d Array

--- runningsum1darray.py
+++ runningsum1darray.py
@@ -2,13 +2,19 @@
 
 Return the running sum of nums.
 
+EXAMPLE 1:
+
 Input: nums = [1,2,3,4]
 Output: [1,3,6,10]
 Explanation: Running sum is obtained as follows: [1, 1+2, 1+2+3, 1+2+3+4].
 
+EXAMPLE 2:
+
 Input: nums = [1,1,1,1,1]
 Output: [1,2,3,4,5]
 Explanation: Running sum is obtained as follows: [1, 1+1, 1+1+1, 1+1+1+1, 1+1+1+1+1].
+
+EXAMPLE 3:
 
 Input: nums = [3,1,2,10,1]
 Output: [3,4,6,16,17]
