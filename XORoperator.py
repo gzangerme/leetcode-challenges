90a130661b522e10a838a9d4fb8be4d662789548
Gabriel Zangerme <gzangerme@gmail.com>
2020-09-16 14:26:50 -0300

    1486. XOR Operation in an Array (Easy)

---
+++ XORoperator.py
@@ -0,0 +1,31 @@
+'''''
+Input: n = 5, start = 0
+Output: 8
+Explanation: Array nums is equal to [0, 2, 4, 6, 8] where (0 ^ 2 ^ 4 ^ 6 ^ 8) = 8.
+Where "^" corresponds to bitwise XOR operator.
+Example 2:
+
+Input: n = 4, start = 3
+Output: 8
+Explanation: Array nums is equal to [3, 5, 7, 9] where (3 ^ 5 ^ 7 ^ 9) = 8.
+Example 3:
+
+Input: n = 1, start = 7
+Output: 7
+Example 4:
+
+Input: n = 10, start = 5
+Output: 2
+
+'''''''
+
+#My solution (42ms)
+
+class Solution:
+    def xorOperation(self, n: int, start: int) -> int:
+        result = start
+        for i in range(1,n):
+            result = result ^ (start + 2*i)
+        return result
+        
+        
